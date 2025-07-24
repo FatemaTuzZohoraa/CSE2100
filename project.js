c73a59a7a75ca8164058f859ed3fe0e5ba50fad9
FatemaTuzZohoraa <fatema.tuz.zohora.070@gmail.com>
2025-07-24 14:07:22 +0600

    .update

--- project.js
+++ project.js
@@ -1,9 +1,53 @@
+//Temperature slider
+let sliderActivated = false;
+document.addEventListener('DOMContentLoaded', () => {
+    const slider = document.getElementById('range');
+    const valueDisplay = document.querySelector('.value');
+    const allElements = document.querySelectorAll('.dv');
+
+    //color update
+  function updateElementStates() {
+    if (!sliderActivated) return; 
+      const temp = parseFloat(slider.value);
+      valueDisplay.textContent = temp.toFixed(0);
+
+      allElements.forEach(el => {
+          const mp = el.dataset.mp ? parseFloat(el.dataset.mp) : null;
+          const bp = el.dataset.bp ? parseFloat(el.dataset.bp) : null;
+
+          
+          el.classList.remove('state-solid', 'state-liquid', 'state-gas', 'state-unknown');
+          
+          
+          if (mp === null || bp === null) {
+              el.classList.add('state-unknown');
+          } else if (temp < mp) {
+              el.classList.add('state-solid');
+          } else if (temp >= mp && temp < bp) {
+              el.classList.add('state-liquid');
+          } else {
+              el.classList.add('state-gas');
+          }
+      });
+  }
+      
+    slider.addEventListener('mousedown', () => {
+    sliderActivated = true;
+    updateElementStates(); 
+    }, { once: true });
+    slider.addEventListener('input', updateElementStates);
+
+    
+    updateElementStates();
+});
+
+//toggle box
 function toggleBox(event, id) {
   const target = event.currentTarget;
   const rect = target.getBoundingClientRect();
   const box = document.getElementById(id);
 
-  // Hide all others
+  
   document.querySelectorAll('.toggle-box').forEach(b => {
     if (b !== box) b.style.display = 'none';
   });
@@ -17,13 +61,13 @@ function toggleBox(event, id) {
     const windowWidth = window.innerWidth;
     const windowHeight = window.innerHeight;
 
-    // Horizontal placement
+    
     let left = rect.right + offset;
     if (left + boxWidth > windowWidth) {
       left = rect.left - boxWidth - offset;
     }
 
-    // Vertical placement
+    
     let top = rect.top + window.scrollY;
     if (top + boxHeight > windowHeight) {
       top = windowHeight - boxHeight - offset;
